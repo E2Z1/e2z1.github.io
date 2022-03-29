36176c1ef5ec89287af1f3fbe0927bd1cea0125a
E2Z1 <ezracentner@gmx.de>
2022-03-29 14:59:28 +0200

    Projects

--- script.js
+++
@@ -1,9 +0,0 @@
-var x = 1
-function buttonaction() {
-    document.getElementById("headline").innerHTML = "Das "+String(x)+". mal gedrückt"
-    x += 1
-}
-function reset() {
-    x = 1
-    document.getElementById("headline").innerHTML = "Kein Score"
-}
