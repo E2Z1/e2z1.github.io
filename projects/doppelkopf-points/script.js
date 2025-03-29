707128e90147e3c075dbc757b54c54cc5fddeb3c
E2Z1 <79697378+E2Z1@users.noreply.github.com>
2025-03-29 14:57:39 +0100

    Update script.js

--- projects/doppelkopf-points/script.js
+++ projects/doppelkopf-points/script.js
@@ -447,7 +447,7 @@ function doStats(data, users) {
     new BarChart("Total Points", totalPoints, document.getElementById("totalPoints"), false);    //title, data, canvas, siPercentage
     new Graph("Point History", individualPointHistory, document.getElementById("totalPointsGraph"));    //title, data, canvas, siPercentage
     new BarChart("Participation", participation, document.getElementById("participation"), true);    //title, data, canvas, siPercentage 
-    new BarChart("Average Win points", winPoints, document.getElementById("winP"), false);    //title, data, canvas, siPercentage
+    new BarChart("Average points", avgPoints, document.getElementById("avgP"), false);    //title, data, canvas, siPercentage
     new BarChart("Average Win points", winPoints, document.getElementById("winP"), false);    //title, data, canvas, siPercentage
     new BarChart("Average Lose points", losePoints, document.getElementById("loseP"), false);    //title, data, canvas, siPercentage
     new BarChart("Wins", wins, document.getElementById("wins"), true);    //title, data, canvas, siPercentage
