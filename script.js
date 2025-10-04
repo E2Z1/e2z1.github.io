07579a6701a5e5b6306f06f84d3b90313ea6f440
E2Z1 <ezracentner@gmx.de>
2025-10-04 19:35:08 +0200

    themes

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 if (window.location.href.startsWith("http://localhost") || window.location.href.startsWith("http://127.0.0.1")) {
-    document.getElementsByTagName("title")[0].innerText = "[DEV] " + document.getElementsByTagName("title")[0].innerText
+    document.getElementsByTagName("title")[0].innerText = "[DEV] " + document.getElementsByTagName("title")[0].innerText;
+}
+if (localStorage.getItem("theme")) {
+	document.body.setAttribute("data-theme", localStorage.getItem("theme"));
 }
 
+const themes = ["default", "catppuccin", "gruvbox", "flashlight"];
+function changeTheme(theme) {
+	localStorage.setItem("theme", theme);
+	document.body.setAttribute("data-theme", theme);
+}
+
+function getTheme() {
+	if (!localStorage.getItem("theme"))
+		return "default";
+	return localStorage.getItem("theme");
+}
+
+function cycleTheme() {
+	let c = themes.indexOf(getTheme());
+	let n = themes[(c+1)%themes.length];
+	changeTheme(n);
+	return n;
+}
